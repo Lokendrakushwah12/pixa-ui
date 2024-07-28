306079efe7dd23a024a628355870a228374c15c6
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-07-29 02:07:34 +0530

    v1

---
+++ src/assets/icons/Airplane.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Airplane = () => {
+  return (
+    <div>
+      <svg
+        className="w-4 h-4"
+        width="21"
+        height="21"
+        viewBox="0 0 21 21"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M6.66659 5.76655L13.7416 3.40822C16.9166 2.34988 18.6416 4.08322 17.5916 7.25822L15.2332 14.3332C13.6499 19.0915 11.0499 19.0915 9.46656 14.3332L8.76659 12.2332L6.66659 11.5332C1.90826 9.94988 1.90826 7.35822 6.66659 5.76655Z"
+          stroke="#0f6fff"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M8.92511 11.875L11.9084 8.8833"
+          stroke="#0f6fff"
+          stroke-width="1.25"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Airplane;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,46 +1,60 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-    return (
-        <>
-            <div className="relative bg-white">
-                <div
-                    className="absolute top-0 z-10 h-[400px] left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-[0.15] pointer-events-none"
-                    style={{ backgroundImage: 'radial-gradient(#A4A4A3, transparent 50%)' }}
-                />
-                <svg
-                    className="absolute pointer-events-none inset-0 h-full w-full stroke-gray-200  [mask-image:radial-gradient(100%_100%_at_top_center,white,transparent)]"
-                    aria-hidden
-                >
-                    <defs>
-                        <pattern
-                            id="s74rfh-fch9w-0879dst"
-                            width={100}
-                            height={100}
-                            x="50%"
-                            y={-1}
-                            patternUnits="userSpaceOnUse"
-                        >
-                            <path d="M100 200V.5M.5 .5H200" fill="none" />
-                        </pattern>
-                    </defs>
-                    <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-                        <path
-                            d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-                            strokeWidth={0}
-                        />
-                    </svg>
-                    <rect width="100%" height="100%" strokeWidth={0} fill="url(#s74rfh-fch9w-0879dst)" />
-                </svg>
-                <div className="max-w-2xl mx-auto text-center py-24">
-                    <div className="flex flex-col relative gap-12">
-                        <h1 className="text-7xl font-semibold mb-4 relative">Pixa UI</h1>
-                        <p className="text-[#5c5c5c] text-xl">A Collection of <span className='font-[500]'>Open Source</span> Components for <span className='border font-[500] rounded-[0.25rem] px-1 bg-[#dedede3f]'>React + TailwindCSS</span> for your Project.</p>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="relative bg-white">
+        <div
+          className="absolute top-0 z-10 h-[400px] left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-[0.15] pointer-events-none"
+          style={{
+            backgroundImage: "radial-gradient(#A4A4A3, transparent 50%)",
+          }}
+        />
+        <svg
+          className="absolute pointer-events-none inset-0 h-full w-full stroke-gray-200  [mask-image:radial-gradient(100%_100%_at_top_center,white,transparent)]"
+          aria-hidden
+        >
+          <defs>
+            <pattern
+              id="s74rfh-fch9w-0879dst"
+              width={100}
+              height={100}
+              x="50%"
+              y={-1}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M100 200V.5M.5 .5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+            <path
+              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+              strokeWidth={0}
+            />
+          </svg>
+          <rect
+            width="100%"
+            height="100%"
+            strokeWidth={0}
+            fill="url(#s74rfh-fch9w-0879dst)"
+          />
+        </svg>
+        <div className="max-w-2xl mx-auto text-center py-24">
+          <div className="flex flex-col relative gap-12">
+            <h1 className="text-7xl font-semibold mb-4 relative">Pixa UI</h1>
+            <p className="text-[#5c5c5c] text-xl">
+              A Collection of <span className="font-[500]">Open Source</span>{" "}
+              Components for &nbsp;
+              <span className="border font-[500] rounded-[0.25rem] bg-[#dedede3f]">
+                React + TailwindCSS
+              </span>{" "}
+              &nbsp; for your Project.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
