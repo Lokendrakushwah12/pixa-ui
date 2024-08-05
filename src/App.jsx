1437ae0d3c2eb97c030e91983ca89937bcb4c011
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-08-05 14:49:10 +0530

    added analytics, speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./pages/Hero";
 import AboutPage from "./pages/AboutPage";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
